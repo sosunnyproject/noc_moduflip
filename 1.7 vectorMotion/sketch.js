28d3215b402a8ab37f6e7bee664f0db23a6073ad
sosunnyproject <parksosun1103@gmail.com>
2019-07-13 00:31:13 +0900

    1.7 with moverClass

--- 1.7 vectorMotion/sketch.js
+++ 1.7 vectorMotion/sketch.js
@@ -2,33 +2,17 @@
 // Daniel Shiffman
 // http://natureofcode.com
 
-// Example 1-2: Bouncing Ball, with p5.Vector!
-let position;
-let velocity;
+let mover;
 
 function setup() {
-  createCanvas(640, 360);
-  background(255);
-  position = createVector(100, 100);
-  velocity = createVector(2.5, 5);
+  createCanvas(640,360);
+  mover = new Mover();
 }
 
 function draw() {
-  background(255);
+  background(51);
 
-  // Add the current speed to the position.
-  position.add(velocity);
-
-  if ((position.x > width) || (position.x < 0)) {
-    velocity.x = velocity.x * -1;
-  }
-  if ((position.y > height) || (position.y < 0)) {
-    velocity.y = velocity.y * -1;
-  }
-
-  // Display circle at x position
-  stroke(0);
-  strokeWeight(2);
-  fill(127);
-  ellipse(position.x, position.y, 48, 48);
+  mover.update();
+  mover.checkEdges();
+  mover.display();
 }
